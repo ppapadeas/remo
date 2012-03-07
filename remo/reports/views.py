7f985643115466d305c417ce2106b1551016d1be
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-03-07 18:34:53 +0200

    Delete comments, delete reports.

--- remo/reports/views.py
+++ remo/reports/views.py
@@ -25,7 +25,6 @@ def view_report(request, display_name, year, month):
                                                       instance=report_comment)
         if report_comment_form.is_valid():
             report_comment_form.save()
-
             messages.success(request, 'Comment saved.')
 
             # provide a new clean form
@@ -44,11 +43,38 @@ def view_report(request, display_name, year, month):
                    'report_comment_form': report_comment_form})
 
 
+# TODO perms
+def delete_report_comment(request, display_name, year, month, comment_id):
+    if request.method == 'POST':
+        if comment_id:
+            report_comment = get_object_or_404(ReportComment, pk=comment_id)
+            report_comment.delete()
+            messages.success(request, 'Comment successfully deleted.')
+
+    report_url = reverse('reports_view_report',
+                         kwargs={'display_name': display_name,
+                                 'year': year,
+                                 'month': month})
+    return redirect(report_url)
+
 def edit_report(request, display_name, year, month):
     user = User.objects.get(userprofile__display_name=display_name)
     year_month = datetime.datetime(year=int(year),
                                    month=utils.month2number(month), day=1)
-    report = utils.get_or_create_instance(Report, user=user, month=year_month)
+
+    # check if it's too early to file this report
+    if year_month.month > datetime.datetime.today().month:
+        messages.error(request, 'I\'m sorry, are you from the future?')
+        if request.user == user:
+            return redirect('profiles_view_my_profile')
+        else:
+            redirect_url = reverse('profiles_view_profile',
+                                   kwargs={'display_name': display_name})
+            return redirect(redirect_url)
+
+
+    report, created = utils.get_or_create_instance(Report, user=user,
+                                                   month=year_month)
 
     if request.method == 'POST':
         report_form = forms.ReportForm(request.POST, instance=report)
@@ -59,11 +85,19 @@ def edit_report(request, display_name, year, month):
 
         if (report_form.is_valid() and report_event_form.is_valid() and
             report_link_form.is_valid()):
-            report_form.save()
-            report_event_form.save()
-            report_link_form.save()
 
-            messages.success(request, 'Report successfully created.')
+            if (not created and report_form.cleaned_data['delete_report']):
+                report.delete()
+                messages.success(request, 'Report successfully deleted.')
+            else:
+                report_form.save()
+                report_event_form.save()
+                report_link_form.save()
+
+                if created:
+                    messages.success(request, 'Report successfully created.')
+                else:
+                    messages.success(request, 'Report successfully updated.')
 
             if request.user == user:
                 return redirect('profiles_view_my_profile')
@@ -72,7 +106,7 @@ def edit_report(request, display_name, year, month):
                                        kwargs={'display_name': display_name})
                 return redirect(redirect_url)
     else:
-        report_form = forms.Reportform(instance=report)
+        report_form = forms.ReportForm(instance=report)
         report_event_form = forms.ReportEventFormset(instance=report)
         report_link_form = forms.ReportLinkFormset(instance=report)
 

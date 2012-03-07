5c8a1b142a074f073ea1754218f1628fdcbea0cc
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-03-07 14:18:21 +0200

    month2number and get_or_create_instance helper functions.

--- remo/base/utils.py
+++ remo/base/utils.py
@@ -1,3 +1,8 @@
+import datetime
+
+from django.http import Http404
+
+
 def latest_object_or_none(model_class, field_name=None):
     """Identical to Model.latest, except instead of throwing exceptions,
     this returns None.
@@ -7,3 +12,18 @@ def latest_object_or_none(model_class, field_name=None):
         return model_class.objects.latest(field_name)
     except (model_class.DoesNotExist, model_class.MultipleObjectsReturned):
         return None
+
+
+def month2number(month):
+    """Convert to month name to number."""
+    try:
+        return datetime.datetime.strptime(month, "%B").month
+    except ValueError:
+        raise Http404
+
+
+def get_or_create_instance(model_class, **kwargs):
+    try:
+        return model_class.objects.get(**kwargs)
+    except model_class.DoesNotExist:
+        return model_class(**kwargs)

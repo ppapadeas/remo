e93c147a4ae7d3c6fa9e9dacc2a30e8a875adc49
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-03-07 18:33:28 +0200

    Add url to delete comments.

--- remo/reports/urls.py
+++ remo/reports/urls.py
@@ -4,5 +4,7 @@ urlpatterns = patterns('remo.reports.views',
     url(r'^(?P<year>\d+)/(?P<month>\w+)/$', 'view_report',
         name='reports_view_report'),
     url(r'^(?P<year>\d+)/(?P<month>\w+)/edit/$', 'edit_report',
-        name='reports_edit_report')
+        name='reports_edit_report'),
+    url(r'^(?P<year>\d+)/(?P<month>\w+)/delete/comment/(?P<comment_id>\d+)/$',
+        'delete_report_comment', name='reports_delete_report_comment'),
 )

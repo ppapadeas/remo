0054fe415b320e47212fca7b5397fec90c298048
Giorgos Logiotatidis <giorgos@mozilla.com>
2012-03-07 18:33:13 +0200

    User number2month function from base.utils.

--- remo/reports/utils.py
+++ remo/reports/utils.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 
+from remo.base.utils import number2month
+
 def get_reports_for_year(user, year):
     """Return a list of reports for year."""
     reports = user.report_set.filter(month__year=year)
     reports_list = []
+
     for month in range(1,13):
-        month_name = datetime(year=2000, day=1, month=month).strftime("%b")
+        month_name = number2month(month, full_name=False)
         month_details = {'name': month_name}
         if reports.filter(month__month=month).exists():
             month_details['report'] = reports.get(month__month=month)
